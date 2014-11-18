227ceeb6d1c547d2e5f8d6d187ebaafa36deed6a
lwissel <lennart.wissel@gmx.net>
2014-11-18 17:27:23 +0000

    add some Grid class prototypes

---
+++ snippets/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Test main
+ *
+ *        Version:  1.0
+ *        Created:  18/11/2014 16:15:20
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Lennart Wissel (lw), lennart.wissel@gmx.net
+ *   Organization:  UoB
+ *
+ * =====================================================================================
+ */
+
+#include "Grid.hpp"
+#include <iostream>
+
+int main() {
+  Grid testgrid(10,10);
+  testgrid.printGrid();
+}
