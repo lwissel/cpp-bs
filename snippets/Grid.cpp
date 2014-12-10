5fa1922811ef9cb4f74a2343fb9b23aed5b6cf9c
lwissel <lennart.wissel@gmx.net>
2014-12-10 19:44:58 +0000

    add doxygen comments

--- snippets/Grid.cpp
+++ snippets/Grid.cpp
@@ -1,5 +1,5 @@
 /*
- * =====================================================================================
+ ***************************************************************************************
  *
  *       Filename:  Grid.cpp
  *
@@ -10,44 +10,62 @@
  *
  *        @author  Lennart Wissel (lw), lennart.wissel@gmx.net
  *
- * =====================================================================================
+ ***************************************************************************************
  */
 
 #include <vector>
 #include <string>
 #include "Grid.hpp"
-/** 
- * @brief default constructor
- *
- * This constructor creates a new Grid of size 10x10 without any ship
- */
+
+/*-----------------------------------------------------------------------------
+ *  constructors
+ *-----------------------------------------------------------------------------*/
+
+/// @brief default constructor
+/// 
+/// This constructor creates a new Grid of size 10x10 without any ship
 Grid::Grid() {
   this->init();
 }
 
-/**
- * @brief constructor to create arbitrary square grids
- */
+///
+/// @brief constructor to create arbitrary square grids
+/// @param size represents the size of the grid
+///
 Grid::Grid(int size) {
   this->init(size);
 }
 
-// private methods
+
+/*-----------------------------------------------------------------------------
+ *  private methods
+ *-----------------------------------------------------------------------------*/
+
+/// @brief initializes a grid with default grid size
 void Grid::init() {
     this->init(this->defaultGridSize);
 }
 
+/// @brief initializes a grid with arbitrary size
+/// @param size represents the size of the grid
 void Grid::init(int size) {
     this->size = size;
     this->grid.resize(size, std::vector<int>(size));
     this->grid.assign(size, std::vector<int>(size,this->FogOfWar));
 }
 
-// public methods
+
+
+/*-----------------------------------------------------------------------------
+ *  public methods
+ *-----------------------------------------------------------------------------*/
 int Grid::getSize() {
     return this->size;
 }
 
+/// @brief prints the grid in raw form
+/// @return string representation of the grid with number representing the
+/// field state
 std::string Grid::printGrid() {
   std::string res = "";
   for ( std::vector<std::vector<int> >::size_type i = 0; i < grid.size(); i++ ) {
